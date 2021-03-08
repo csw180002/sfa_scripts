6c429c3d72de3afcced3f2fc2b95f6858ab7746b
csw180002 <80228275+csw180002@users.noreply.github.com>
2021-03-07 19:10:28 -0600

    Add files via upload
    In this version I added the path and from path library. I did this so that the pathing for any file library format is going to be a problem.

--- scenefile.py
+++ scenefile.py
@@ -1,13 +1,28 @@
+from pathlib import Path
+
+
 class SceneFile(object):
     """An abrstact representation of a scene file"""
 
     def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = folder_path
+        self.folder_path = Path(folder_path)
         self.descriptor = descriptor
         self.task = task
         self.ver = ver
         self.ext = ext
 
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
 
-scene_file = SceneFile("D:\\", "tank", "model", "v001", ".ma")
-print(scene_file.descriptor)
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+
+
+scene_file = SceneFile("D:/", "tank", "model", 1, ".ma")
+print(scene_file.path)
